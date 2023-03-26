de2490742eeaa09b308141bfcc6ebf3224498f5f
pkellner <peter@peterkellner.net>
2023-03-26 06:31:51 -0700

    updated code to demonstrate why this is still a bug and should be fixed.

--- src/app/demo/HeaderDemo.tsx
+++ src/app/demo/HeaderDemo.tsx
@@ -1,14 +1,16 @@
-const delay = (ms: number) => {
-  console.log("delay:",ms)
-  return new Promise((resolve) => setTimeout(resolve, ms));
-};
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
 
-async function getDateAndTimeDelayed() {
-  await delay(2000);
-  return new Date().toDateString();
+async function getRandomNumber() {
+  await new Promise(resolve => setTimeout(resolve, 2000));
+  const randomInt = getRandomInt(1000,3000);
+  return randomInt;
 }
 
 export default async function HeaderDemo() {
-  const now = await getDateAndTimeDelayed();
-  return <div>on {now}</div>;
+  const num = await getRandomNumber();
+  return <div>Random Number:{num}</div>;
 }
